7250839e59ef0a7d979665b5a0eabcf1094cb8c9
Ben E <benjamin.eng7@gmail.com>
2016-10-05 11:43:30 -0700

    remove open weather api

--- app/controllers/temperatures_controller.rb
+++ app/controllers/temperatures_controller.rb
@@ -1,18 +1,13 @@
 class TemperaturesController < ApplicationController
   require 'area'
-  require 'open_weather'
 
   def show
     @zip = params[:zip]
     @city = @zip.to_region
-
-    #from open weather
-    temp_payload = OpenWeather::Current.city(@city, units: "imperial")
-    @name = temp_payload['name']
-    @faren = temp_payload['main']['temp']
-    @cels = (((@faren-32) * 5)/9).round(2)
-
-    #from wunerground
+    @lat = params[:zip].to_lat
+    @lon = params[:zip].to_lon
+    
+    #from wunderground
     w_api = Wunderground.new("1aa6d90f5543a8cc")
     wunder = w_api.conditions_for("#{@zip}")['current_observation']
     @faren_w = wunder['temp_f']
